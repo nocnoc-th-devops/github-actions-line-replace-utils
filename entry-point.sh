97dedf80feeb464923d730b16e657cac14d5d5db
Athip Rooprayochsilp <>
2023-03-23 21:51:48 +0700

    Fix the errors

--- entry-point.sh
+++ entry-point.sh
@@ -6,7 +6,11 @@ REPLACE_VALUE=$3
 REPLACE_FILE=$4
 
 if [ "$REPLACE_FILE" == "blank.txt" ]; then
+  echo "text<<EOF" >> $GITHUB_OUTPUT
   /replace "$INPUT_FILE" "$LINE_PATTERN" value "$REPLACE_VALUE" >> $GITHUB_OUTPUT
+  echo "EOF" >> $GITHUB_OUTPUT
 else
+  echo "text<<EOF" >> $GITHUB_OUTPUT
   /replace "$INPUT_FILE" "$LINE_PATTERN" file "$REPLACE_FILE" >> $GITHUB_OUTPUT
+  echo "EOF" >> $GITHUB_OUTPUT
 fi
